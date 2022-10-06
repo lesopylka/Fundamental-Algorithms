25ed524c6d3e8c30eda9f497170e38a6943e775f
Alina <darersthr@gmail.com>
2022-10-07 00:51:43 +0300

    upd t3.c

--- 2/t3.c
+++ 2/t3.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <stdarg.h>
+
+unsigned int fileSymbolsCount(char *fileName){
+    FILE* input_file = fopen(fileName, "r");
+        if(input_file == NULL) {
+            printf("Error: file cannot be open.\n");
+        }
+        char header;
+        unsigned int count = 0;
+        while ((header = fgetc(input_file)) != EOF) {
+                    if (header != '\n' && header != '\t') {
+                        count++;
+                    }
+                }
+    return(count);
+}
  
 void ilyaneebipls(char* str, char* fileName, ...) {
     va_list factor;         // указатель на необязательный параметр
