f9f546d5a99e22bd76421d79d2075d907ea43b7f
Alina <darersthr@gmail.com>
2022-10-06 23:29:17 +0300

    upd t2

--- 2/t2.c
+++ 2/t2.c
@@ -7,35 +7,36 @@
 
 int main() {
 
-    switch (expression)
-    {
-        case 'l': 
-             printf("Length of string: %d\n", length(argv[1]));
-            break;
+    char* parametrs [5] = {"-l", "-r", "-u", "-n", "-c"};
+    int good_flag = 0, flag;
 
-        case 'r':
-            reverse(argv[1]);
-            printf("Reverse: %s\n", argv[1]);
-            break;
-
-        case 'u':
-           upper_register_through_2(argv[1]);
-            printf("Result: %s\n", argv[1]);
-            break;
-
-        case 'n':
-            printf("Replaced: %s\n",replace(argv[1]));
-            break;
-
-        case 'c':
-            if (argc >= 4) {
-                printf("%s", concat(argv[1], argv[3]));
-            } else {
-                printf("Need one more argument!");
+    for (flag = 0; flag < 5; ++flag) {
+        if (!strcmp(parametrs[flag], argv[2])) {
+            good_flag = 1;
+            switch (parametrs[flag][1]) {
+                case 'l':
+                    printf("Length of string: %d\n", length(argv[1]));
+                    break;
+                case 'r':
+                    reverse(argv[1]);
+                    printf("Reverse: %s\n", argv[1]);
+                    break;
+                case 'u':
+                    upper_register_through_2(argv[1]);
+                    printf("Result: %s\n", argv[1]);
+                    break;
+                case 'n':
+                    printf("Replaced: %s\n",replace(argv[1]));
+                    break;
+                case 'c':
+                    if (argc >= 4) {
+                        printf("%s", concat(argv[1], argv[3]));
+                    } else {
+                        printf("Need one more argument!");
+                    }
+                    break;
             }
             break;
-
-        default:
-            break;
+        }
     }
 }
