fc62a8a8aa483436c20c586c7ea2f0d22737be74
Alina <darersthr@gmail.com>
2022-10-07 00:53:59 +0300

    upd t3.c->t6.c

---
+++ 2/t6.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+unsigned int fileSymbolsCount(char *fileName){
+    FILE* input_file = fopen(fileName, "r");
+        if(input_file == NULL) {
+            printf("Error: file cannot be open.\n");
+        }
+        char header;
+        unsigned int count = 0;
+        while ((header = fgetc(input_file)) != EOF) {
+                    if (header != '\n' && header != '\t') {
+                        count++;
+                    }
+                }
+    return(count);
+}
+ 
+void ilyaneebipls(char* str, char* fileName, ...) {
+    va_list factor;         // указатель на необязательный параметр
+    va_start(factor, fileName);   // устанавливаем указатель
+
+    for(char *c = fileName; *c; c++){
+        FILE* input_file = fopen(fileName, "r");
+        if(input_file == NULL) {
+            printf("Error: file cannot be open.\n");
+        }
+        char header = ';
+        while ((header = fgetc(input_file)) != EOF) {
+                if (!(isdigit(c))) {
+
+                }
+            }
+    }
+    va_end(factor);
+}
+
+int main() {   
+    ilyaneebipls();
+    return 0;
+}
