d75c983e0e268563d3062c708231afb72ff6b25f
Alina <darersthr@gmail.com>
2022-10-09 00:20:28 +0300

    upd t9.c

---
+++ 2/t9.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void reverse(char* str){
+    int n = length(str);
+    for(int i = 0; i < n / 2; ++i)
+    swap(str, i, n - 1 - i);
+    return;
+}
+
+void swap(){
+    return;
+}
+
+int length(){
+    return 0;
+}
+
+int ToInteger(){
+    return 0;
+}
+
+int ToChar(){
+    return 0;
+}
+
+char SumInCC(){
+    return 0;
+}
+
+int main(){
+    return 0;
+}
