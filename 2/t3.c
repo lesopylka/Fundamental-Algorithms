e3c871bc6527dac97ff7906a801d040477a1891b
Alina <darersthr@gmail.com>
2022-11-02 18:38:33 +0300

    edit+now 3/

--- 2/t3.c
+++ 2/t3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 #define _CRT_SECURE_NO_WARNINGS
 #define _USE_MATH_DEFINES
 
@@ -12,337 +13,313 @@ long double sqrt2_lim(double eps);
 long double euler_lim(double eps);
 
 long double _fabs(long double x) {
-    return x > 0 ? x : -x;
+  return x > 0 ? x : -x;
 }
 
-long double limit(long double lim(int n), double eps)
-{
-	int i = 1 / eps / eps, step = 1 / eps;
-	long double res = 0, prev_res = 0;
-	eps *= eps;
+long double limit(long double lim(int n), double eps) {
+  int i = 1 / eps / eps, step = 1 / eps;
+  long double res = 0, prev_res = 0;
+  eps *= eps;
 
-	while (_fabs(res - prev_res) > eps || prev_res == 0) {
-		prev_res = res;
-		res = lim(i += step);
-	}
+  while (_fabs(res - prev_res) > eps || prev_res == 0) {
+    prev_res = res;
+    res = lim(i += step);
+  }
 
-	return res;
+  return res;
 }
 
-long double sum(long double function(int n), double eps, int start)
-{
-	long double res = 0, prev_res = 0;
-	for (int k = start; (_fabs(res - prev_res) > eps || _fabs(prev_res) < eps || (res - prev_res == 0)); k++) {
-		prev_res = res;
-		res += function(k);
-	}
-	return res;
+long double sum(long double
+  function (int n), double eps, int start) {
+  long double res = 0, prev_res = 0;
+  for (int k = start;
+    (_fabs(res - prev_res) > eps || _fabs(prev_res) < eps || (res - prev_res == 0)); k++) {
+    prev_res = res;
+    res += function (k);
+  }
+  return res;
 }
 
-long double mult(long double function(int n), double eps, int start)
-{
-	long double res = 1, prev_res = 0, eps_func = eps / 10;
-	for (int k = start; _fabs(prev_res - res) > eps_func || _fabs(prev_res) < eps; k++) {
-		prev_res = res;
-		res *= function(k);
-	}
-	return res;
+long double mult(long double
+  function (int n), double eps, int start) {
+  long double res = 1, prev_res = 0, eps_func = eps / 10;
+  for (int k = start; _fabs(prev_res - res) > eps_func || _fabs(prev_res) < eps; k++) {
+    prev_res = res;
+    res *= function (k);
+  }
+  return res;
 }
 
-long double factorial(int num)
-{
-	if (num == 0) {
-		return 1;
-	}
-	int res = 1;
-	while (num > 0) {
-		res *= num;
-		num--;
-	}
-	return res;
+long double factorial(int num) {
+  if (num == 0) {
+    return 1;
+  }
+  int res = 1;
+  while (num > 0) {
+    res *= num;
+    num--;
+  }
+  return res;
 }
 
-long double e_lim(int n)
-{
-	return pow(1 + 1.0 / n, n);
+long double e_lim(int n) {
+  return pow(1 + 1.0 / n, n);
 }
 
-long double pi_lim(int n)
-{
-	int tmp = n;
-	long double res = 1;
-	while (tmp) {
-		res *= 4.0 * tmp * tmp / (2 * tmp) / (2 * tmp - 1);
-		tmp--;
-	}
-	res /= sqrt(n);
-	return res *= res;
+long double pi_lim(int n) {
+  int tmp = n;
+  long double res = 1;
+  while (tmp) {
+    res *= 4.0 * tmp * tmp / (2 * tmp) / (2 * tmp - 1);
+    tmp--;
+  }
+  res /= sqrt(n);
+  return res *= res;
 }
 
-long double ln2_lim(int n)
-{
-	return n * (pow(2, 1.0 / n) - 1);
+long double ln2_lim(int n) {
+  return n * (pow(2, 1.0 / n) - 1);
 }
 
-long double sqrt2_lim(double eps)
-{
-	int i = 0, j;
-	long double res = -0.5, prev_res = 0;
-	eps *= eps;
+long double sqrt2_lim(double eps) {
+  int i = 0, j;
+  long double res = -0.5, prev_res = 0;
+  eps *= eps;
 
-	while (_fabs(res - prev_res) > eps || prev_res == 0) {
-		prev_res = res;
-		res = res - ((res * res) / 2) + 1;
-	}
+  while (_fabs(res - prev_res) > eps || prev_res == 0) {
+    prev_res = res;
+    res = res - ((res * res) / 2) + 1;
+  }
 
-	return res;
+  return res;
 }
 
-long double e_row(int n)
-{
-	return 1.0 / factorial(n);
+long double e_row(int n) {
+  return 1.0 / factorial(n);
 }
 
-long double pi_row(int n)
-{
-	return 4 * pow(-1.0, n - 1) / (2.0 * n - 1);
+long double pi_row(int n) {
+  return 4 * pow(-1.0, n - 1) / (2.0 * n - 1);
 }
 
-long double ln2_row(int n)
-{
-	return pow(-1.0, n - 1) / n;
+long double ln2_row(int n) {
+  return pow(-1.0, n - 1) / n;
 }
 
-long double sqrt2_mult(int k)
-{
-	return pow(2, pow(2, -k));
+long double sqrt2_mult(int k) {
+  return pow(2, pow(2, -k));
 }
 
-long double gamma_row(double eps, int start)
-{
-	long double res = 0, eps_func = 1e-15, prev_res = 0, func_returned = 0;
+long double gamma_row(double eps, int start) {
+  long double res = 0, eps_func = 1e-15, prev_res = 0, func_returned = 0;
 
-	for (int k = start; (_fabs(res - prev_res) > eps_func || _fabs(prev_res) < eps || (res - prev_res == 0)); k++) {
-		prev_res = res;
-		func_returned = 1.0 / pow(floor(sqrt(k)), 2) - 1.0 / k;
-		res += func_returned;
-	}
-	return res;
+  for (int k = start;
+    (_fabs(res - prev_res) > eps_func || _fabs(prev_res) < eps || (res - prev_res == 0)); k++) {
+    prev_res = res;
+    func_returned = 1.0 / pow(floor(sqrt(k)), 2) - 1.0 / k;
+    res += func_returned;
+  }
+  return res;
 }
 
-double equation_dichotomy(double equation(double x), double a, double b, double eps)
-{
-	double funct_result_middle;
-	while (_fabs(a - b) > eps) {
-		funct_result_middle = equation((a + b) / 2);
-		if (equation(a) * funct_result_middle > 0) {
-			a = (a + b) / 2;
-		}
+double equation_dichotomy(double equation(double x), double a, double b, double eps) {
+  double funct_result_middle;
+  while (_fabs(a - b) > eps) {
+    funct_result_middle = equation((a + b) / 2);
+    if (equation(a) * funct_result_middle > 0) {
+      a = (a + b) / 2;
+    }
 
-		if (equation(b) * funct_result_middle > 0) {
-			b = (a + b) / 2;
-		}
-	}
-	return (a + b) / 2;
+    if (equation(b) * funct_result_middle > 0) {
+      b = (a + b) / 2;
+    }
+  }
+  return (a + b) / 2;
 }
 
-double e_equation(double x)
-{
-	return log(x) - 1;
+double e_equation(double x) {
+  return log(x) - 1;
 }
 
-double pi_equation(double x)
-{
-	return -sin(x);
+double pi_equation(double x) {
+  return -sin(x);
 }
 
-double ln2_equation(double x)
-{
-	return pow(M_E, x) - 2;
+double ln2_equation(double x) {
+  return pow(M_E, x) - 2;
 }
 
-double sqrt2_equation(double x)
-{
-	return x * x - 2;
+double sqrt2_equation(double x) {
+  return x * x - 2;
 }
 
-void simple_nums_generate(long** simple_nums, unsigned long long to_generate, unsigned long long* generated)
-{
-	int count = 0;
-	long *tmp = NULL, i, j, * array_sieve = NULL;
+void simple_nums_generate(long ** simple_nums, unsigned long long to_generate, unsigned long long * generated) {
+  int count = 0;
+  long * tmp = NULL, i, j, * array_sieve = NULL;
 
-	if (!(array_sieve = (long*)calloc((to_generate + 1), sizeof(long)))) {
-		return;
-	}
+  if (!(array_sieve = (long * ) calloc((to_generate + 1), sizeof(long)))) {
+    return;
+  }
 
-	for (int i = 2; i <= to_generate; i++) {
-		if (!array_sieve[i]) {
-			count++;
-			for (int j = i + i; j <= to_generate; j += i) {
-				array_sieve[j] = 1;
-			}
-		}
-	}
-	
-	if ((*simple_nums == NULL || (*generated) == 0)) {
-		if (!((*simple_nums) = (long*)calloc(count, sizeof(long)))) {
-			return;
-		}
-	}
-	else {
-		if (!(tmp = (long*)realloc((*simple_nums), sizeof(long) * (count + (*generated))))) {
-			free(*simple_nums);
-			return;
-		}
-		(*simple_nums) = tmp;
-	}
-	
-	i = (!(*generated) ? 2 : (*simple_nums)[(*generated) - 1] + 1);
-	for (int i = 0; i <= to_generate; i++) {
-		if (!array_sieve[i]) {
-			(*simple_nums)[(*generated)++] = i;
-		}
-	}
+  for (int i = 2; i <= to_generate; i++) {
+    if (!array_sieve[i]) {
+      count++;
+      for (int j = i + i; j <= to_generate; j += i) {
+        array_sieve[j] = 1;
+      }
+    }
+  }
 
-	free(array_sieve);
+  if (( * simple_nums == NULL || ( * generated) == 0)) {
+    if (!(( * simple_nums) = (long * ) calloc(count, sizeof(long)))) {
+      return;
+    }
+  } else {
+    if (!(tmp = (long * ) realloc(( * simple_nums), sizeof(long) * (count + ( * generated))))) {
+      free( * simple_nums);
+      return;
+    }
+    ( * simple_nums) = tmp;
+  }
+
+  i = (!( * generated) ? 2 : ( * simple_nums)[( * generated) - 1] + 1);
+  for (int i = 0; i <= to_generate; i++) {
+    if (!array_sieve[i]) {
+      ( * simple_nums)[( * generated) ++] = i;
+    }
+  }
+
+  free(array_sieve);
 
 }
 
-double euler_equation(double eps)
-{
-	double result = 1, prev_result = 0;
-	int step = 1. / eps / 100;
-	long* simple_nums = NULL;
-	unsigned long long t = 1.0 / eps / eps, generated = 0, i;
-	eps /= 1000;
+double euler_equation(double eps) {
+  double result = 1, prev_result = 0;
+  int step = 1. / eps / 100;
+  long * simple_nums = NULL;
+  unsigned long long t = 1.0 / eps / eps, generated = 0, i;
+  eps /= 1000;
 
-	for (int t = 0; _fabs(result - prev_result) > eps; t += step) {
-		prev_result = result;
-		result = 1;
-		simple_nums_generate(&simple_nums, t, &generated);
-		for (int i = 0; i < generated; i++) {
-			result *= (double)(simple_nums[i] - 1) / simple_nums[i];
-		}
-		result *= log(t);
-		result = -log(result);
-	}
+  for (int t = 0; _fabs(result - prev_result) > eps; t += step) {
+    prev_result = result;
+    result = 1;
+    simple_nums_generate( & simple_nums, t, & generated);
+    for (int i = 0; i < generated; i++) {
+      result *= (double)(simple_nums[i] - 1) / simple_nums[i];
+    }
+    result *= log(t);
+    result = -log(result);
+  }
 
-	free(simple_nums);
-	return result;
+  free(simple_nums);
+  return result;
 }
 
-void pascal_table_generate(unsigned long long*** table, int* generated, int to_generate)
-{
-	unsigned long long** tmp_table = NULL, * tmp_string = NULL;
-	if (!(*generated)) {
-		if (!((**table) = (unsigned long long*)malloc(sizeof(unsigned long long)))) {
-			return;
-		}
-		(*table)[0][0] = 1;
-		(*generated)++;
-	}
+void pascal_table_generate(unsigned long long ** * table, int * generated, int to_generate) {
+  unsigned long long ** tmp_table = NULL, * tmp_string = NULL;
+  if (!( * generated)) {
+    if (!(( ** table) = (unsigned long long * ) malloc(sizeof(unsigned long long)))) {
+      return;
+    }
+    ( * table)[0][0] = 1;
+    ( * generated) ++;
+  }
 
-	// gen enough strings
-	if (!(tmp_table = (unsigned long long**)realloc((*table), sizeof(unsigned long long*) * (to_generate + 1)))) {
-		return;
-	}
-	(*table) = tmp_table;
+  // gen enough strings
+  if (!(tmp_table = (unsigned long long ** ) realloc(( * table), sizeof(unsigned long long * ) * (to_generate + 1)))) {
+    return;
+  }
+  ( * table) = tmp_table;
 
-	int str, col, string_items_count;
-	for (str = (*generated); str < to_generate + 1; str++) {
-		// gen array (string) with enough memory for items
-		string_items_count = str + 1;
-		if (!((*table)[str] = (unsigned long long*)malloc(sizeof(unsigned long long) * string_items_count))) {
-			return;
-		}
-		
-		for (col = 0; col <= str; col++) {
-			if (col == 0 || col == str) {
-				(*table)[str][col] = 1;
-			}
+  int str, col, string_items_count;
+  for (str = ( * generated); str < to_generate + 1; str++) {
+    // gen array (string) with enough memory for items
+    string_items_count = str + 1;
+    if (!(( * table)[str] = (unsigned long long * ) malloc(sizeof(unsigned long long) * string_items_count))) {
+      return;
+    }
 
-			else {
-				(*table)[str][col] = (*table)[str - 1][col - 1] + (*table)[str - 1][col];
-			}
-		}
-	}
-	(*generated) = str;
+    for (col = 0; col <= str; col++) {
+      if (col == 0 || col == str) {
+        ( * table)[str][col] = 1;
+      } else {
+        ( * table)[str][col] = ( * table)[str - 1][col - 1] + ( * table)[str - 1][col];
+      }
+    }
+  }
+  ( * generated) = str;
 }
 
-long double euler_lim(double eps)
-{
-	unsigned long long** pascal_table = NULL;
-	if (!(pascal_table = (unsigned long long**)calloc(1, sizeof(long*)))) {
-		return NAN;
-	}
+long double euler_lim(double eps) {
+  unsigned long long ** pascal_table = NULL;
+  if (!(pascal_table = (unsigned long long ** ) calloc(1, sizeof(long * )))) {
+    return NAN;
+  }
 
-	int k, m = 32, i = 0, generated_strings = 0;
-	long double partial_sum = 1, log_sum = 0, res = 0, prev_res = 0;
-	eps /= 100;
+  int k, m = 32, i = 0, generated_strings = 0;
+  long double partial_sum = 1, log_sum = 0, res = 0, prev_res = 0;
+  eps /= 100;
 
-	while (_fabs(prev_res - res) > eps || _fabs(prev_res - res) == 0) {
-		prev_res = res;
-		res = 0;
-		log_sum = 0;
-		partial_sum = 1;
+  while (_fabs(prev_res - res) > eps || _fabs(prev_res - res) == 0) {
+    prev_res = res;
+    res = 0;
+    log_sum = 0;
+    partial_sum = 1;
 
-		pascal_table_generate(&pascal_table, &generated_strings, m);
+    pascal_table_generate( & pascal_table, & generated_strings, m);
 
-		for (int k = 1; k <= m; k++) {
-			partial_sum *= (long double)pascal_table[m][k] / k;
-			partial_sum *= (long double)(k & 1 ? -1 : 1);
+    for (int k = 1; k <= m; k++) {
+      partial_sum *= (long double) pascal_table[m][k] / k;
+      partial_sum *= (long double)(k & 1 ? -1 : 1);
 
-			log_sum += log(k);
+      log_sum += log(k);
 
-			res += log_sum * partial_sum;
-			partial_sum = 1;
-		}
-		m++;
-	}
+      res += log_sum * partial_sum;
+      partial_sum = 1;
+    }
+    m++;
+  }
 
-	for (int i = 0; i < generated_strings; i++) {
-		free(pascal_table[i]);
-	}
-	free(pascal_table);
+  for (int i = 0; i < generated_strings; i++) {
+    free(pascal_table[i]);
+  }
+  free(pascal_table);
 
-	return res;
+  return res;
 }
 
+int main(void) {
+  int l;
+  printf("Enter precision:\n");
+  scanf("%d", & l);
+  double eps = pow(10, -l);
+  l++;
 
+  printf("e constant:\n");
+  printf("\tas limit value: %.*Lf\n", l, limit(e_lim, eps));
+  printf("\tas row sum: %.*Lf\n", l, sum(e_row, eps, 0));
+  printf("\tas solution of an equation: %.*lf\n\n", l, equation_dichotomy(e_equation, 2., 3., eps));
 
-int main(void)
-{
-	int l;
-	printf("Enter precision:\n");
-	scanf("%d", &l);
- 	double eps = pow(10, -l);
-	l++; 
-	
-	printf("e constant:\n");
-	printf("\tas limit value: %.*Lf\n", l, limit(e_lim, eps));
-	printf("\tas row sum: %.*Lf\n", l, sum(e_row, eps, 0));
-	printf("\tas solution of an equation: %.*lf\n\n", l, equation_dichotomy(e_equation, 2., 3., eps));
+  printf("pi constant:\n");
+  printf("\tas limit value: %.*Lf\n", l, limit(pi_lim, eps));
+  printf("\tas row sum: %.*Lf\n", l, sum(pi_row, eps, 1));
+  printf("\tas solution of an equation: %.*lf\n\n", l, equation_dichotomy(pi_equation, 3, 3.5, eps));
 
-	printf("pi constant:\n");
-	printf("\tas limit value: %.*Lf\n", l, limit(pi_lim, eps));
-	printf("\tas row sum: %.*Lf\n", l, sum(pi_row, eps, 1));
-	printf("\tas solution of an equation: %.*lf\n\n", l, equation_dichotomy(pi_equation, 3, 3.5, eps));
-	
-	printf("ln(2) constant:\n");
-	printf("\tas limit value: %.*Lf\n", l, limit(ln2_lim, eps));
-	printf("\tas row sum: %.*Lf\n", l, sum(ln2_row, eps, 1));
-	printf("\tas solution of an equation: %.*lf\n\n", l, equation_dichotomy(ln2_equation, 0, 1, eps));
+  printf("ln(2) constant:\n");
+  printf("\tas limit value: %.*Lf\n", l, limit(ln2_lim, eps));
+  printf("\tas row sum: %.*Lf\n", l, sum(ln2_row, eps, 1));
+  printf("\tas solution of an equation: %.*lf\n\n", l, equation_dichotomy(ln2_equation, 0, 1, eps));
 
-	printf("sqrt(2) constant:\n");
-	printf("\tas limit value: %.*Lf\n", l, sqrt2_lim(eps));
-	printf("\tas row product: %.*Lf\n", l, mult(sqrt2_mult, eps, 2));
-	printf("\tas solution of an equation: %.*lf\n\n", l, equation_dichotomy(sqrt2_equation, 1, 2, eps));
-	
-	printf("Euler's constant:\n");
-	printf("\tas limit value: %.*Lf\n", l, euler_lim(eps));
-	double constant_pi = M_PI * M_PI / 6;
-	printf("\tas row sum: %.*Lf\n", l, gamma_row(eps, 2) - constant_pi);
-	printf("\tas solution of an equation: %.*lf\n\n", l, euler_equation(eps));
+  printf("sqrt(2) constant:\n");
+  printf("\tas limit value: %.*Lf\n", l, sqrt2_lim(eps));
+  printf("\tas row product: %.*Lf\n", l, mult(sqrt2_mult, eps, 2));
+  printf("\tas solution of an equation: %.*lf\n\n", l, equation_dichotomy(sqrt2_equation, 1, 2, eps));
 
-	return 0;
+  printf("Euler's constant:\n");
+  printf("\tas limit value: %.*Lf\n", l, euler_lim(eps));
+  double constant_pi = M_PI * M_PI / 6;
+  printf("\tas row sum: %.*Lf\n", l, gamma_row(eps, 2) - constant_pi);
+  printf("\tas solution of an equation: %.*lf\n\n", l, euler_equation(eps));
+
+  return 0;
 }
--- 3/t4.c
+++ 3/t4.c
@@ -4,215 +4,229 @@
 
 
 typedef struct {
-    unsigned int id;
-    unsigned int size;
-    char *text;
-} message;
+  unsigned int id;
+  unsigned int size;
+  char * text;
+}
+message;
 
-typedef message *ListType;
+typedef message * ListType;
 
 typedef struct {
-    ListType *data;
-    int size;
-    int capacity;
-} List;
+  ListType * data;
+  int size;
+  int capacity;
+}
+List;
 
-short addToList(List *list, ListType data) {
-    if (list->size == list->capacity) {
-        list->capacity *= 2;
-        ListType *newData = (ListType *) realloc(list->data, sizeof(ListType) * (list->capacity));
-        if (newData == NULL) {
-            list->capacity /= 2;
-            return 0;
-        }
-        list->data = newData;
+short addToList(List * list, ListType data) {
+  if (list -> size == list -> capacity) {
+    list -> capacity *= 2;
+    ListType * newData = (ListType * ) realloc(list -> data, sizeof(ListType) * (list -> capacity));
+    if (newData == NULL) {
+      list -> capacity /= 2;
+      return 0;
     }
-    list->data[list->size] = data;
-    list->size++;
-    return 1;
+    list -> data = newData;
+  }
+  list -> data[list -> size] = data;
+  list -> size++;
+  return 1;
 }
 
-void destroyList(List *list) {
-    for (int x = 0; x < list->size; x++) {
-        free(list->data[x]);
-    }
-    free(list->data);
-    free(list);
+void destroyList(List * list) {
+  for (int x = 0; x < list -> size; x++) {
+    free(list -> data[x]);
+  }
+  free(list -> data);
+  free(list);
 }
 
-List *createList() {
-    List *list = (List *) malloc(sizeof(List));
-    ListType *data = (ListType *) malloc(sizeof(ListType) * 5);
-    if (list == NULL || data == NULL) {
-        return NULL;
-    }
-    list->size = 0;
-    list->data = data;
-    list->capacity = 5;
-    return list;
+List * createList() {
+  List * list = (List * ) malloc(sizeof(List));
+  ListType * data = (ListType * ) malloc(sizeof(ListType) * 5);
+  if (list == NULL || data == NULL) {
+    return NULL;
+  }
+  list -> size = 0;
+  list -> data = data;
+  list -> capacity = 5;
+  return list;
 }
 
-short genFilename(char **result, char *extension) {
-    int len = 11 + strlen(extension);
-    char *res = (char *) malloc(sizeof(char) * len);
-    if (res == NULL) {
-        return 0;
-    }
-    res[len - 1] = '\0';
-    for (int x = 0; x < 10; x++) {
-        int ch = rand() % 36;
-        if (ch < 10)
-            res[x] = ch + '0';
-        else
-            res[x] = ch - 10 + 'a';
-    }
-    for (int x = 10; x < len - 1; x++) {
-        res[x] = extension[x - 10];
-    }
-    *result = res;
-    return 1;
-}
-
-short getMessage(char *res, int resCapacity) {
-    char c;
-    int count = 0;
-    while ((c = getchar()) != '\n') {
-        if (count == resCapacity - 1) {
-            return 1;
-        }
-        if (!(
-                ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') ||
-                ('0' <= c && c <= '9') || (c == ' ') || (c == '.') || (c == ',') ||
-                (c == '"') || (c == '?') || (c == ':') || (c == ';') || (c == '-') ||
-                (c == '!') || (c == '(') || (c == ')') || (c == '=') || (c == '*')
-        )) {
-            return 2;
-        }
-        res[count] = c;
-        count++;
-    }
-    res[count] = '\0';
+short genFilename(char ** result, char * extension) {
+  int len = 11 + strlen(extension);
+  char * res = (char * ) malloc(sizeof(char) * len);
+  if (res == NULL) {
     return 0;
+  }
+  res[len - 1] = '\0';
+  for (int x = 0; x < 10; x++) {
+    int ch = rand() % 36;
+    if (ch < 10)
+      res[x] = ch + '0';
+    else
+      res[x] = ch - 10 + 'a';
+  }
+  for (int x = 10; x < len - 1; x++) {
+    res[x] = extension[x - 10];
+  }
+  * result = res;
+  return 1;
 }
 
-void writeMessage(FILE *file, char *text, int id) {
-    char *c;
-    c = (char *) &id;
-    for (int x = 0; x < sizeof(int); x++) {
-        putc(*c++, file);
+short getMessage(char * res, int resCapacity) {
+  char c;
+  int count = 0;
+  while ((c = getchar()) != '\n') {
+    if (count == resCapacity - 1) {
+      return 1;
     }
-    int len = strlen(text);
-    c = (char *) &len;
-    for (int x = 0; x < sizeof(int); x++) {
-        putc(*c++, file);
+    if (!(
+        ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') ||
+        ('0' <= c && c <= '9') || (c == ' ') || (c == '.') || (c == ',') ||
+        (c == '"') || (c == '?') || (c == ':') || (c == ';') || (c == '-') ||
+        (c == '!') || (c == '(') || (c == ')') || (c == '=') || (c == '*')
+      )) {
+      return 2;
     }
-    c = (char *) text;
+    res[count] = c;
+    count++;
+  }
+  res[count] = '\0';
+  return 0;
+}
+
+void writeMessage(FILE * file, char * text, int id) {
+  char * c;
+  c = (char * ) & id;
+  for (int x = 0; x < sizeof(int); x++) {
+    putc( * c++, file);
+  }
+  int len = strlen(text);
+  c = (char * ) & len;
+  for (int x = 0; x < sizeof(int); x++) {
+    putc( * c++, file);
+  }
+  c = (char * ) text;
+  for (int x = 0; x < len; x++) {
+    putc( * c++, file);
+  }
+}
+
+short loadMessages(List ** result, FILE * in , int count) {
+  int id, len;
+  List * list = createList();
+  if (list == NULL) {
+    return 1;
+  }
+  for (int i = 0; i < count; i++) {
+    char * c;
+    c = (char * ) & id;
+    for (int x = 0; x < sizeof(int); x++) {
+      * c = getc( in );
+      c++;
+    }
+    len = 0;
+    c = (char * ) & len;
+    for (int x = 0; x < sizeof(int); x++) {
+      * c = getc( in );
+      c++;
+    }
+    char * text = (char * ) malloc(sizeof(char) * (len + 1));
+    if (text == NULL) {
+      destroyList(list);
+      return 1;
+    }
+    c = (char * ) text;
     for (int x = 0; x < len; x++) {
-        putc(*c++, file);
+      * c = getc( in );
+      c++;
     }
+    * c = '\0';
+    message * mes = (message * ) malloc(sizeof(message));
+    if (mes == NULL) {
+      destroyList(list);
+      return 2;
+    }
+    mes -> id = id;
+    mes -> size = len + 1;
+    mes -> text = text;
+    if (!addToList(list, mes)) {
+      destroyList(list);
+      free(text);
+      return 3;
+    }
+  }
+  * result = list;
+  return 0;
 }
 
-short loadMessages(List **result, FILE *in, int count) {
-    int id, len;
-    List *list = createList();
-    if (list == NULL) {
-        return 1;
-    }
-    for (int i = 0; i < count; i++) {
-        char *c;
-        c = (char *) &id;
-        for (int x = 0; x < sizeof(int); x++) {
-            *c = getc(in);
-            c++;
-        }
-        len = 0;
-        c = (char *) &len;
-        for (int x = 0; x < sizeof(int); x++) {
-            *c = getc(in);
-            c++;
-        }
-        char *text = (char *) malloc(sizeof(char) * (len + 1));
-        if (text == NULL) {
-            destroyList(list);
-            return 1;
-        }
-        c = (char *) text;
-        for (int x = 0; x < len; x++) {
-            *c = getc(in);
-            c++;
-        }
-        *c = '\0';
-        message *mes = (message *) malloc(sizeof(message));
-        if (mes == NULL) {
-            destroyList(list);
-            return 2;
-        }
-        mes->id = id;
-        mes->size = len + 1;
-        mes->text = text;
-        if (!addToList(list, mes)) {
-            destroyList(list);
-            free(text);
-            return 3;
-        }
-    }
-    *result = list;
-    return 0;
+void printList(List * mes) {
+  printf("\n\n");
+  for (int x = 0; x < mes -> size; x++) {
+    printf("id: %d\n", mes -> data[x] -> id);
+    printf("len: %d\n", mes -> data[x] -> size);
+    printf("text: %s\n\n", mes -> data[x] -> text);
+  }
 }
 
-void printList(List *mes) {
-    printf("\n\n");
-    for (int x = 0; x < mes->size; x++) {
-        printf("id: %d\n", mes->data[x]->id);
-        printf("len: %d\n", mes->data[x]->size);
-        printf("text: %s\n\n", mes->data[x]->text);
-    }
-}
+int main(int argc, char * argv[]) {
+  if (argc != 2)
+    return 69;
 
-int main(int argc, char *argv[]) {
-    if (argc != 2)
-        return 69;
+  FILE * data;
+  char * filename;
+  int id = 0;
 
-    FILE *data;
-    char *filename;
-    int id = 0;
-
-    if (!genFilename(&filename, ".csv")) {
-        return 1;
-    }
-    if ((data = fopen(filename, "wb")) == NULL) {
-        free(filename);
-        return 3;
-    }
-
-    char message[100];
-
-    while (1) {
-        printf("Enter message %d\n", id + 1);
-        switch (getMessage(message, 100)) {
-            case 1: free(filename); fclose(data); return 4;
-            case 2: free(filename); fclose(data); return 5;
-        }
-        if (strcmp(message, argv[1]) == 0)
-            break;
-        writeMessage(data, message, id);
-        id++;
-    }
-    fclose(data);
-
-    if ((data = fopen(filename, "rb")) == NULL) {
-        free(filename);
-        return 3;
-    }
+  if (!genFilename( & filename, ".csv")) {
+    return 1;
+  }
+  if ((data = fopen(filename, "wb")) == NULL) {
     free(filename);
-    List *messages;
-    switch (loadMessages(&messages, data, id)) {
-        case 1: fclose(data); return 9;
-        case 2: fclose(data); return 10;
-        case 3: fclose(data); return 11;
+    return 3;
+  }
+
+  char message[100];
+
+  while (1) {
+    printf("Enter message %d\n", id + 1);
+    switch (getMessage(message, 100)) {
+    case 1:
+      free(filename);
+      fclose(data);
+      return 4;
+    case 2:
+      free(filename);
+      fclose(data);
+      return 5;
     }
-    printList(messages);
-    destroyList(messages);
+    if (strcmp(message, argv[1]) == 0)
+      break;
+    writeMessage(data, message, id);
+    id++;
+  }
+  fclose(data);
+
+  if ((data = fopen(filename, "rb")) == NULL) {
+    free(filename);
+    return 3;
+  }
+  free(filename);
+  List * messages;
+  switch (loadMessages( & messages, data, id)) {
+  case 1:
     fclose(data);
-    return 0;
+    return 9;
+  case 2:
+    fclose(data);
+    return 10;
+  case 3:
+    fclose(data);
+    return 11;
+  }
+  printList(messages);
+  destroyList(messages);
+  fclose(data);
+  return 0;
 }
