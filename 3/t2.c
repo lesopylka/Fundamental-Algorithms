4129edd5d6a0154a20cdcb01ccb14058803065f1
Alina <darersthr@gmail.com>
2022-11-19 21:02:43 +0300

    start

---
+++ 3/t2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int numBits(){
+
+}
+
+int factorial(){
+
+}
+
+int getMem(){
+ 
+}
+
+int checkBits(){
+   
+}
+
+int bitsIn(){
+   
+}
+
+int bitsForward(){
+
+}
+
+int print(){
+   
+}
+
+int main(){
+
+}
