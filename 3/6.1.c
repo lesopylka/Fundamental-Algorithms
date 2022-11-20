fe3b739907dca5f64ed3ddfa44a4316e90f36ecc
Alina <darersthr@gmail.com>
2022-11-20 13:20:58 +0300

    пошла пить пиво

---
+++ 3/6.1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int comp(){
+
+}
+
+int freeAll(){ 
+
+}
+
+int createString(){ 
+
+}
+
+int clearString(){ 
+     
+}
+
+int compareStrings(){ 
+
+}
+
+int freeString(){ 
+
+}
+
+int copyString(){
+
+}
+
+int concatStrings(){ 
+
+}
+
+int duplicateString(){ 
+
+}
+
+int main(){
+
+}
