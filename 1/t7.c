93e73099f837876860fff3c80b9c21664a1284ba
Alina <darersthr@gmail.com>
2022-12-04 11:44:40 +0300

    memory sampling correctness

--- 1/t7.c
+++ 1/t7.c
@@ -5,47 +5,48 @@
 #include <time.h>
 
 void Swap(int * a, int * b) {
-  int c;
-  c = * a;
-  * a = * b;
-  * b = c;
+    a +=b;
+    b = a - b;
+    a = a - b;
+    //   int c;
+    //   c = * a;
+    //   * a = * b;
+    //   * b = c;
 }
 
 int main(void) {
-  int n = 20, i, j;
-  int * matrix = (int * ) malloc(sizeof(int) * n);
-  srand(time(0));
-  int min, max, nmin = 0, nmax = 0;
-  for (i = 0; i < n; i++) {
-    matrix[i] = (rand() % 100) * (rand() % 8 == 0 ? -1 : 1);
-    printf("%d ", matrix[i]);
-
-    if (i == 0) {
-      max = matrix[i];
-      min = matrix[i];
-    } else {
-      if (matrix[i] > max) {
-        max = matrix[i];
-        nmax = i;
-      } else if (matrix[i] < min) {
-        min = matrix[i];
-        nmin = i;
-      }
+    int n = 20, i, j;
+    int * matrix = (int * ) malloc(sizeof(int) * n);
+    if (matrix == NULL) exit(1);
+    srand(time(0));
+    int min, max, min_indx = 0, max_indx = 0;
+    max = INT_MIN;
+    min = INT_MAX;
+    for (i = 0; i < n; i++) {
+        matrix[i] = (rand() % 100) * (rand() % 8 == 0 ? -1 : 1);
+        printf("%d ", matrix[i]);
+        if (matrix[i] > max) {
+            max = matrix[i];
+            max_indx = i;
+        }
+        if (matrix[i] < min) {
+            min = matrix[i];
+            min_indx = i;
+        }
     }
-  }
-  Swap( & matrix[nmin], & matrix[nmax]);
-  printf(" min = %d, max = %d\n", min, max);
+    Swap( & matrix[nmin], & matrix[nmax]);
+    printf(" min = %d, max = %d\n", min, max);
 
-  for (i = 0; i < n; i++) {
-    printf("%d ", matrix[i]);
-  }
-  printf(" — result tranfer");
+    for (i = 0; i < n; i++) {
+        printf("%d ", matrix[i]);
+    }
+    printf(" — result tranfer");
 
-  free(matrix);
+    free(matrix);
 
-  return 0;
+    return 0;
 }
 
 
-// 16 строка: проверки на корректность выделения памяти нет, пофиксить с возможностью обработки ситуации в вызывающем коде
+// 16 строка: проверки на корректность выделения памяти нет, пофиксить с возможностью обработки ситуации в вызывающем коде+
 // 36 строка: вместо &a[i] лучше написать a + i, -2 операции на использование сразу
