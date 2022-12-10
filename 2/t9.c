b271d39e9b25dedd9b698ea177eda385b0771fc5
Alina <darersthr@gmail.com>
2022-12-10 21:00:53 +0300

    вот бы домой

--- 2/t9.c
+++ 2/t9.c
@@ -1,8 +1,3 @@
-// Написать функцию с переменным числом аргументов, вычисляющую сумму переданных чисел в заданной системе счисления. 
-// Примечание: десятичное представление чисел может быть слишком велико и не поместиться в типы int, long int и т.д.
-//  Для решения возникшей проблемы напишите функцию «сложения в столбик» чисел в заданной системе счисления в 
-//  строковом представлении.
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -10,39 +5,46 @@
 #include <ctype.h>
 #include <string.h>
 
-int max(int a, int b) {
-    if (a >= b) return a;
-    else return b;
-    }
-
 
 int length(const char *str) {
     int i = 0;
-    while (str[++i]) return 0;
+    while (str[++i]);
+    return 0;
 }
 
 int ToInteger(char c) {
-    if (c >= 'A' && c <= 'Z') return c - 'A' + 10;
-    else if (c >= 'a' && c <= 'z') return c - 'a' + 10;
-    else if (c >= '0' && c <= '9') return c - '0';
+    if (c >= 'A' && c <= 'Z') 
+        return c - 'A' + 10;
+    else if (c >= 'a' && c <= 'z') 
+        return c - 'a' + 10;
+    else if (c >= '0' && c <= '9') 
+        return c - '0';
     return 0;
 }
 
 int ToChar(char c) {
-    if (c >= 10 && c <= 26) return c + 'A' - 10;
-    else if (c >= 0 && c <= 9) return c + '0';
+    if (c >= 10 && c <= 26)
+        return c + 'A' - 10;
+    else if (c >= 0 && c <= 9)
+        return c + '0';
     else return -1;
 }
 
 char *Stacking(char *str1, char *str2, int n) {
     int len1 = strlen(str1);
     int len2 = strlen(str2);
-    int len_result = max(len1, len2) + 2;
+    int len_result = ((len1 > len2) ? len1: len2) + 2;
     char *result = (char *) malloc(sizeof(char) * len_result);
-    if (*result = NULL) {return 1;}
+    if (*result = NULL) 
+    {
+        return 1;
+    }
     for (int i = 0; i < len_result - 1; ++i) result[i] = '0';
     result[len_result - 1] = '\0';
-    int p[] = { len1 - 1, len2 - 1 };
+    int p[] = {
+            len1 - 1,
+            len2 - 1
+    };
     int s = 0;
     for (int i = len_result - 2; i >= 0; --i) {
 
@@ -57,13 +59,30 @@ char *Stacking(char *str1, char *str2, int n) {
     return result;
 }
 
+char* summ(int system_num, unsigned num, char* first, ...) 
+{
+    num = num-2;
+    char **p = &first;
+    char* sum = Stacking(*p,*(++p), system_num);
+    p++;
+    while (num--)
+    {
+        char* tmp = Stacking(sum,*p, system_num);
+        memcpy(sum, tmp, strlen(tmp));
+        free(tmp);
+        p++;
+    }
+    return sum;
+}
 
 
 int main() {
     int n = 2;
-    char *str1 = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE";
-    char *str2 = "0000000000000002"; //функция с переменным числом аргументов
-    char *res = Stacking(str1, str2, n);
+    char str1[] = "00001";
+    char str2[] = "0000000010";
+    char str3[] = "0000010000";
+    char str4[] = "0010000000";
+    char *res = summ(2,4,&str1,&str2,&str3,&str4);//Stacking(&str1,&str2,2);
 
     printf("%s\n", res);
     free(res);
